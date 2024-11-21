53745a4eafa39ffb2550462488e526f27128623c
Karthiksai Kumaraguru <karthiksaiguru@gmail.com>
2024-11-21 12:47:59 +0530

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,9 +1,9 @@
 def ispangram(str):
 	alph = "abcdefghijklmnopqrstuvwxyz"
 	for i in alph:
-		if i not in str.lower():  ''' .lower() eliminates the need to consider capital alphabets '''
+		if i not in str.lower():
 			return False
-	return True               ''' true will only be returned if the entire for loop executes without executing the if block. '''
+	return True
 
 str = input()
 if(ispangram(str) == True):
